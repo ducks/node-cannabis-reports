5a60065e4e68dd294dbb298db7fbfb71e04f06af
Jake Goldsborough <rjgoldsborough@gmail.com>
2016-10-22 08:52:12 -0700

    removing jsdoc and starting to implement raml

---
+++ raml/data/strains/strains.js
@@ -0,0 +1,54 @@
+{
+  "data": [
+    {
+      "name": "501st OG",
+      "ucpc": "4FMDMKJYQL000000000000000",
+      "link": "https://www.cannabisreports.com/api/v1.0/strains/4FMDMKJYQL000000000000000",
+      "qr": "https://www.cannabisreports.com/strain-reports/rare-dankness-seeds/501st-og/qr-code.svg",
+      "url": "https://www.cannabisreports.com/strain-reports/rare-dankness-seeds/501st-og",
+      "image": "https://www.cannabisreports.com/images/strains/a/full_aac791c4ba5e08993b92130a8d95cff56cd3e242.jpg",
+      "seedCompany": {
+        "name": "Rare Dankness Seeds",
+        "ucpc": "4FMDM00000000000000000000",
+        "link": "https://www.cannabisreports.com/api/v1.0/seed-companies/4FMDM00000000000000000000"
+      },
+      "genetics": {
+        "names": "Skywalker OG x Rare Dankness #1",
+        "ucpc": "9XVU7ZDGYL000000000000000 x 4FMDM9DTWX000000000000000",
+        "link": "https://www.cannabisreports.com/api/v1.0/strains/4FMDMKJYQL000000000000000/genetics"
+      },
+      "lineage": {
+        "Afghanistan": "AF"
+      },
+      "children": {
+        "count": 0,
+        "link": "https://www.cannabisreports.com/api/v1.0/strains/4FMDMKJYQL000000000000000/children"
+      },
+      "reviews": {
+        "count": 0,
+        "link": "https://www.cannabisreports.com/api/v1.0/strains/4FMDMKJYQL000000000000000/reviews"
+      },
+      "createdAt": {
+        "datetime": "2014-10-01 17:12:35",
+        "timezone": "UTC"
+      },
+      "updatedAt": {
+        "datetime": "2014-10-01 17:12:35",
+        "timezone": "UTC"
+      }
+    },
+  ],
+  "meta": {
+    "pagination": {
+      "total": 4738,
+      "count": 10,
+      "per_page": 10,
+      "current_page": 3,
+      "total_pages": 474,
+      "links": {
+        "previous": "https://www.cannabisreports.com/api/v1.0/strains?sort=name&page=2",
+        "next": "https://www.cannabisreports.com/api/v1.0/strains?sort=name&page=4"
+      }
+    }
+  }
+}
